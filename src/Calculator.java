a092d6ba8dc7affd1895cb678c285b9deb802d74
Keerthijasthi <keerthi.jasthi4@gmail.com>
2025-05-15 17:00:50 -0500

    Code added to enter a valid number.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,22 +1,42 @@
+//ask the input
+//checking if it is number
+//return the error
+// need to throw the enter the valid number
+
 import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the first number: ");
-    double num1 = sc.nextDouble();
-    System.out.println("Enter the second number: ");
-    double num2 = sc.nextDouble();
-    double addition = num1 + num2;
-    System.out.println("Addition: " + addition);
-    double subtraction = num1 - num2;
-    System.out.println("Subtraction: " + subtraction);
-    double multiplication = num1 * num2;
-    System.out.println("Multiplication: " + multiplication);
-    if(num2 !=0){
-        double division = num1 / num2;
-        System.out.println("The Division is: " + division);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = getValidNumber(sc, "Enter the first number: ");
+        double num2 = getValidNumber(sc, "Enter the second number: ");
+
+        double addition = num1 + num2;
+        System.out.println("Addition: " + addition);
+
+        double subtraction = num1 - num2;
+        System.out.println("Subtraction: " + subtraction);
+
+        double multiplication = num1 * num2;
+        System.out.println("Multiplication: " + multiplication);
+
+        if (num2 != 0) {
+            double division = num1 / num2;
+            System.out.println("The Division is: " + division);
         } else {
-        System.out.println("Division: Cannot divide by zero!");
+            System.out.println("Division: Cannot divide by zero!");
+        }
     }
+    private static double getValidNumber(Scanner sc, String prompt) {
+        while (true) {
+            System.out.print(prompt);
+            if (sc.hasNextDouble()) {
+                return sc.nextDouble();
+            } else {
+                System.out.println("Enter a valid number.");
+                sc.next();
+            }
+        }
     }
 }
