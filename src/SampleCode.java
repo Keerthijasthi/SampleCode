f831a9457dfa3d95f7bf73ffc8f98740461b73b5
Keerthijasthi <keerthi.jasthi4@gmail.com>
2025-05-15 12:12:37 -0500

    Initial commit

---
+++ src/SampleCode.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SampleApplication {
+    public static void main(String[] args) {
+        System.out.println(isValid("HOWDY"));
+        System.out.println(isValid("HOODY"));
+        System.out.println(isValid("ROWDY"));
+    }
+    public static boolean isValid(String input) {
+        String allowed = "HOWDY";
+        Set<Character> seen = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (allowed.indexOf(ch) == -1) {
+                return false;
+            }
+            if (seen.contains(ch)) {
+                return false;
+            }
+            seen.add(ch);
+        }
+        return true;
+    }
+}
