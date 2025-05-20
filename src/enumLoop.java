f6cb57ac7c7c2d46652fed715b0387830696d3c2
Keerthijasthi <keerthi.jasthi4@gmail.com>
2025-05-20 11:27:27 -0500

    tasks of Day3.

--- src/enumLoop.java
+++ src/enumLoop.java
@@ -1,23 +1,34 @@
 // Define the enum
 enum Subject {
-    MATH,
-    SCIENCE,
-    ENGLISH,
-    HISTORY,
-    COMPUTER
+    MATH("Mathematics"),
+    SCIENCE("Science"),
+    ENGLISH("English Literature"),
+    HISTORY("World History"),
+    COMPUTER("Computer Science");
+
+    private final String fullName;
+    // Constructor
+    Subject(String fullName) {
+        this.fullName = fullName;
+    }
+
+    // Getter method to access full name
+    public String getFullName() {
+        return fullName;
+    }
 }
 public class enumLoop {
     public static void main(String[] args) {
-        System.out.print("List of subjects in enum: [" + Subject.MATH.toString());
-        System.out.print(", " + Subject.SCIENCE.toString());
-        System.out.print(", " + Subject.ENGLISH.toString());
-        System.out.print(", " + Subject.HISTORY.toString());
-        System.out.println(", " + Subject.COMPUTER.toString() + "]");
+//        System.out.print("List of subjects in enum: [" + Subject.MATH.toString());
+//        System.out.print(", " + Subject.SCIENCE.toString());
+//        System.out.print(", " + Subject.ENGLISH.toString());
+//        System.out.print(", " + Subject.HISTORY.toString());
+//        System.out.println(", " + Subject.COMPUTER.toString() + "]");
 
         System.out.println("List of all subjects using enhanced for loop:");
         // Loop through all enum values using Subject.values()
         for (Subject subject : Subject.values()) {
-            System.out.println(subject);
+            System.out.println(subject.getFullName());
         }
     }
 }
